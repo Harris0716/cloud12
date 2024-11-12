82321991c3747756457e2c09f27c90306a22c3f6
Cash Yang <CashYang16@gmail.com>
2024-11-12 10:24:58 +0800

    modify login frontend

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 
 import React,{useState}from "react";
+import { useNavigate } from "react-router-dom";
 import "./HomePage.css";
 
 const listings = [
@@ -35,6 +36,7 @@ const listings = [
 
 function HomePage() {
     const [searchQuery, setSearchQuery] = useState("");
+    const navigate = useNavigate();
 
     const filteredListings = listings.filter((listing) =>
       listing.title.toLowerCase().includes(searchQuery.toLowerCase())
@@ -42,9 +44,13 @@ function HomePage() {
   
     return (
       <div className="container">
-        <h1 className="title">熱門房源</h1>
-        
-        {/* 搜尋欄位 */}
+        <div className="homepage">
+          <div className="header">
+            <button className="login-button" onClick={() => navigate("/login")}>Login</button>
+          </div>
+          <br /><br /><br />
+          <h1 className="title">熱門房源</h1>
+          {/* 搜尋欄位 */}
         <div className="search-bar">
           <input
             type="text"
@@ -54,7 +60,8 @@ function HomePage() {
             className="search-input"
           />
         </div>
-  
+        </div>
+        {/* 房源列表 */}
         <div className="listing-grid">
           {filteredListings.map((listing) => (
             <div key={listing.id} className="listing-card">
