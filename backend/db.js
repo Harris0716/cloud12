d258f40df1a62042e1f84c19f00ae471a0c978f9
Cash Yang <CashYang16@gmail.com>
2024-11-25 18:32:10 +0800

    use dotenv package to read environment variables

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,9 @@
 // Establish connection to the mysql database
+require("dotenv").config();
 const mysql = require("mysql2");
 var connection = mysql.createPool({
   host: process.env.HOST,
-  user: process.env.USER,
+  user: process.env.USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
 });
