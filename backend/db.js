d8f610908a2d72a6a29378bc9b781a7beb4e525b
Harris Guan <guan99716@gmail.com~>
2024-12-12 15:08:18 +0800

    fix: remove DB connection test in db.js

--- backend/db.js
+++ backend/db.js
@@ -8,15 +8,4 @@ const connection = mysql.createPool({
   database: process.env.DATABASE,
 });
 
-// test DB connection
-connection.promise().query('SELECT 1')
-  .then(() => {
-    console.log('資料庫連線成功！');
-  })
-  .catch(err => {
-    console.error('資料庫連線失敗:', err);
-    process.exit(1);
-  });
-
-
 module.exports = connection;
