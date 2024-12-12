59240221c58068a2ada584835c8545b5973e30fa
Cash Yang <CashYang16@gmail.com>
2024-12-12 13:13:42 +0800

    add docker compose to backend

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,13 @@
 // Establish connection to the mysql database
 require("dotenv").config();
+const PORT = process.env.DB_PORT || 3306;
 const mysql = require("mysql2");
 var connection = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
+  port: PORT,
 });
 
 module.exports = connection;
