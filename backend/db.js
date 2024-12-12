13505d1548801ef7cfbb60f3030d720889355dea
茶碗蒸 <zhenyuliu0825@gmail.com>
2024-12-12 14:18:58 +0800

    Fix: update the db connection

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const mysql = require("mysql2");
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'kevin6290',
-  database: 'my_database',
+  host: process.env.HOST,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 
 
@@ -14,18 +14,6 @@ connection.getConnection((err, conn) => {
     console.error("Error connecting to database:", err);
     return;
   }
-
-  // 可以開始使用 conn 來進行 SQL 查詢
-  conn.query("SELECT * FROM user", (err, results) => {
-    if (err) {
-      console.error("Query failed:", err);
-    } else {
-      console.log("Query results:", results);
-    }
-
-    // 最後記得釋放連接回池中
-    conn.release();
-  });
 });
 
 module.exports = connection;
