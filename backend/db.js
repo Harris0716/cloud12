f9124c6913643c9a408a70f6d34017b07b8a5ec2
Kevin Liu <58145495+Lzzz666@users.noreply.github.com>
2024-12-12 14:07:13 +0800

    Fixed: delete not use fuction

--- backend/db.js
+++ backend/db.js
@@ -14,18 +14,6 @@ connection.getConnection((err, conn) => {
     console.error("Error connecting to database:", err);
     return;
   }
-
-  // 可以開始使用 conn 來進行 SQL 查詢
-  conn.query("SELECT * FROM user", (err, results) => {
-    if (err) {
-      console.error("Query failed:", err);
-    } else {
-      console.log("Query results:", results);
-    }
-
-    // 最後記得釋放連接回池中
-    conn.release();
-  });
 });
 
 module.exports = connection;
