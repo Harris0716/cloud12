f8bd153227ef381f316191a3afe61e7b6a7a417e
Cash Yang <CashYang16@gmail.com>
2024-12-19 14:58:38 +0800

    add status to getLandlordApplications

--- backend/application/model.js
+++ backend/application/model.js
@@ -63,7 +63,7 @@ function getApplierApplications(applier_id) {
 function getLandlordApplications(landlord_id) {
   const params = [landlord_id];
   const sql = `
-    SELECT a.application_id, a.message, j.cover_image, j.positions, u.username
+    SELECT a.application_id, a.message, a.status, j.cover_image, j.positions, u.username
     FROM Application as a, JobInfo as j, User as u 
     WHERE a.landlord_id = ? and j.jobInfo_id = a.job_id and u.user_id = a.applier_id`;
   return new Promise((resolve, reject) => {
