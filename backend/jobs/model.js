3ea8288553d9bd6f1d0ffa81ed7b12e35d548d53
andy059120 <113753121@g.nccu.edu.tw>
2024-12-31 11:59:19 +0800

    modify: jobinfo api

--- backend/jobs/model.js
+++ backend/jobs/model.js
@@ -104,4 +104,43 @@ function getJobById(jobInfo_id) {
   });
 }
 
-module.exports = { listJobs, getJobById };
+function createJobInfo(jobInfoData) {
+  const query = `
+      INSERT INTO JobInfo 
+      (landlord_id, address, room_type, start_date, end_date, job_description, positions, people_needed, cover_image, detail_images, benefits) 
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+
+  const values = [
+    jobInfoData.landlord_id,
+    jobInfoData.address,
+    jobInfoData.room_type,
+    jobInfoData.start_date,
+    jobInfoData.end_date,
+    jobInfoData.job_description,
+    jobInfoData.positions,
+    jobInfoData.people_needed,
+    jobInfoData.cover_image,
+    jobInfoData.detail_images,
+    jobInfoData.benefits,
+  ];
+
+  return new Promise((resolve, reject) => {
+    db.getConnection((err, connection) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      connection.query(query, values, (err, results) => {
+        connection.release();
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  });
+}
+
+module.exports = { listJobs, getJobById, createJobInfo };
--- backend/jobs/route.js
+++ backend/jobs/route.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
-const { list, getJobDetail } = require("./controller");
+const { list, getJobDetail, post_jobinfo } = require("./controller");
+const authenticateToken = require("../user/authMiddleware");
 
 // 工作列表
 router.get("/api/jobs", list);
 
 // 工作詳細資訊
 router.get("/api/job/:jobInfo_id", getJobDetail);
+
+// 新增工作
+router.post("/api/jobinfo", authenticateToken, post_jobinfo);
 
 module.exports = router;
