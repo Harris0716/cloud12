ecffd61373e94a3e4342ba141bf6fa3ff412c427
Guan <112753134@g.nccu.edu.tw>
2025-01-01 14:47:19 +0800

    delete backend/landlord

--- backend/landlord/route.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { post_jobinfo } = require("./controller");
-const authenticateToken = require("../user/authMiddleware");
-
-
-router.post("/api/jobinfo", authenticateToken, post_jobinfo);
-
-module.exports = router;
