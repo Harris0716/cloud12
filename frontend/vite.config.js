7e7bac574164c2e15fcf949ded30e4be8dd177b6
Guan <112753134@g.nccu.edu.tw>
2025-01-03 18:28:44 +0800

    feat:add api_ip to .env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,16 +7,5 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-  },
-  // 加入環境變數的配置
-  define: {
-    __API_URL__: JSON.stringify(import.meta.env.VITE_API_URL),
-  },
-  // 加入開發時的代理配置（如果需要的話）
-  // proxy: {
-  //   '/api': {
-  //     target: 'http://54.238.10.84:8000',
-  //     changeOrigin: true,
-  //   }
-  // }
+  }
 });
