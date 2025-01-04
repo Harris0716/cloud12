2c85e921854b04b1dce7db8114f832486bde6c47
Cash Yang <CashYang16@gmail.com>
2025-01-04 15:48:13 +0800

    fix variable error

--- frontend/src/components/Application/ApplicationForm.jsx
+++ frontend/src/components/Application/ApplicationForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./ApplicationForm.css";
-const api_base_url = import.meta.env.VITE_API_BASE_URL;
+const api_base_url = import.meta.env.VITE_API_URL;
 
 function ApplicationForm({ jobInfo_id }) {
     const [dateError, setDateError] = useState("");
